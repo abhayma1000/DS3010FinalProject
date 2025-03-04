bcc078c8db5288f7e0f1b2e9fe9828fe907085b2
Abhay Mathur <abhaymathur1000@gmail.com>
2025-03-04 09:03:48 -0500

    A lot of stuff including starting pt 2. The pipeline works, but it is not yielding corect results. Going to refactor'

--- tmp.py
+++ tmp.py
@@ -1,23 +1,12 @@
-import spacy
- 
-# Load the spaCy English model
-nlp = spacy.load('en_core_web_sm')
- 
-def lemmatize(text):
-    # Process the text using spaCy
-    doc = nlp(text)
-     
-    # Extract lemmatized tokens
-    lemmatized_tokens = [token.lemma_ for token in doc]
-     
-    # Join the lemmatized tokens into a sentence
-    lemmatized_text = ' '.join(lemmatized_tokens)
+# import time
 
-    return lemmatized_text
+# the_time = time.time()
 
-# Define a sample text
-the_text = "The quick brown foxes are jumping over the lazy dogs."
- 
-# Print the original and lemmatized text
-print("Original Text:", the_text)
-print("Lemmatized Text:", lemmatize(the_text))
+# time.sleep(5)
+
+# elapsed_time = time.time() - the_time
+# hours, rem = divmod(elapsed_time, 3600)
+# minutes, seconds = divmod(rem, 60)
+# print(f"{int(hours):02}:{int(minutes):02}:{seconds:05.2f}")
+
+print(11 * 60 / 6)
